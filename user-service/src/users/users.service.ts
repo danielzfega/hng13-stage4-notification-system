3a0768cfb539e904c2798f000444a8e95c3c1ca1
Bayode <bayodeoluwatosin06@gmail.com>
2025-11-10 15:38:38 +0100

    feat(user): setup user service with prisma and basic CRU operation

---
+++ user-service/src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  createUser(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  findAllUsers() {
+    return this.prisma.user.findMany({ include: { preferences: true } });
+  }
+
+  updateUser(id: string, data: Prisma.UserUpdateInput) {
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+}
